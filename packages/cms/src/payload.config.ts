bd0b754d5374b06d6909aebd0ba8230c0ada365e
pisitch1 <pisitch@gmail.com>
2024-11-11 06:08:40 -0800

    feat: vercel blob

--- packages/cms/src/payload.config.ts
+++ packages/cms/src/payload.config.ts
@@ -8,6 +8,7 @@ import sharp from 'sharp'
 
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
+import { vercelBlobStorage } from '@payloadcms/storage-vercel-blob'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -32,6 +33,12 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    // storage-adapter-placeholder
+    vercelBlobStorage({
+      collections: {
+        [Media.slug]: true,
+      },
+      // Token provided by Vercel once Blob storage is added to your Vercel project
+      token: process.env.BLOB_READ_WRITE_TOKEN || '',
+    }),
   ],
 })
