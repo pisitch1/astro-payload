bedd24f1f63c78505799c625e462aabfa9599c05
pisitch1 <pisitch@gmail.com>
2024-11-11 17:07:45 -0800

    feat: image optimization

--- packages/web/astro.config.mjs
+++ packages/web/astro.config.mjs
@@ -1,10 +1,22 @@
 // @ts-check
 // @ts-check
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
 
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  image: {
+    domains: ['localhost:3000'], // Add your domain
+    // Or use remotePatterns for more flexible matching
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3000',
+        pathname: '/api/media/**',
+      },
+    ],
+  },
 });
